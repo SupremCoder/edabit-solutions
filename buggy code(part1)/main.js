751d1ecb8dd8b0a20489cedbc3bc9bf8d51fef53
Jasurbek <jasurbektolibov@gmail.com>
2023-11-08 23:01:42 +0500

    all js files deleted instead of this js codes is written in html file

--- buggy code(part1)/main.js
+++
@@ -1,5 +0,0 @@
-let cub__side = +prompt('Enter cubes side')
-function cubes(a) {
-    return Math.pow(a, 3)
-}
-console.log(cubes(cub__side));
--- perim of rect/main.js
+++
@@ -1,6 +0,0 @@
-let length = +prompt('Enter property length of rectangle')
-let width = +prompt('Enter property width of rectangle')
-function findPerimeter(a, b) {
-    return 2 * (a + b)
-}
-console.log(findPerimeter(length, width))
